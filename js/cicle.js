e4eb8a3d222730a3a01dbe5c795662cbd3dc2d79
Oksana Bilovus <o.top@ukr.net>
2016-11-10 08:21:06 +0000

    h

---
+++ js/cicle.js
@@ -0,0 +1,12 @@
+var n = Number(prompt('ВВЕДІТЬ n'));
+function fib(n) {
+  var a = 1,
+    b = 1;
+  for (var i = 3; i <= n; i++) {
+    var c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+document.write( fib(n) );
